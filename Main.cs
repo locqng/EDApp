f400c325771a3b49c09d7f9c14fae9a251f4e641
Yuan Fang <89305420+Ellie1105@users.noreply.github.com>
2021-08-25 20:36:08 +1000

    Add function buttons & change the form size, add start position function, & relocation the text field & label position.

--- Main.cs
+++ Main.cs
@@ -15,22 +15,30 @@ namespace EDApp{
         public void menuFrame()
         {
             //Set the form width & height 
-            this.Width = 500;
-            this.Height = 500;
+            this.Width = 800;
+            this.Height = 600;
 
-             //Creating the top panel
+            //Set the start position of the form to the center of the screen
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            //Creating the top panel
             topPanel = new Panel();
-            topPanel.Width = 500;
-            topPanel.Height = 20;
+            topPanel.Width = 800;
+            topPanel.Height = 30;
             
             //Creating the main panel
             mainPanel = new Panel();
-            mainPanel.Width = 500;
-            mainPanel.Height = 480;
+            mainPanel.Width = 800;
+            mainPanel.Height = 580;
             
             //Generating Menu buttons
             Button btnEdit = new Button();
             Button btnView = new Button();
+
+            //Generating Function buttons 
+            Button btnAdd = new Button();
+            Button btnDelete = new Button();
+            Button btnExit = new Button();
             
             //Generating form labels
             Label lblID = new Label();
@@ -55,102 +63,119 @@ namespace EDApp{
             TextBox txbDoc = new TextBox();
 
             //Setting menu buttons size and locations
-
             //Edit button
             btnEdit.Location = new Point(0,0);
             btnEdit.Text = "Edit";
-            btnEdit.Size = new Size(250,20);
+            btnEdit.Size = new Size(400,30);
             btnEdit.Click += new System.EventHandler(btnEditClick);
 
             //View button          
-            btnView.Location = new Point(250,0);
+            btnView.Location = new Point(400,0);
             btnView.Text = "View";
-            btnView.Size = new Size(250,20);
+            btnView.Size = new Size(400,30);
             btnView.Click += new System.EventHandler(btnViewClick);
 
-            //Setting labels and text boxes size and location
+            //Add button 
+            btnAdd.Location = new Point(20,500);
+            btnAdd.Text = "Add";
+            btnAdd.Size = new Size(80,20);
+            //btnAdd.Click += new System.EventHandler(btnAddClick);
+            
+            //Delete button 
+            btnDelete.Location = new Point(130,500);
+            btnDelete.Text = "Delete";
+            btnDelete.Size = new Size(80,20);
+            //btnDelete.Click += new System.EventHandler(btnDeleteClick);
 
+            //Exit button 
+            btnExit.Location = new Point(680,500);
+            btnExit.Text = "Exit";
+            btnExit.Size = new Size(80,20);
+            //btnExit.Click += new System.EventHandler(btnExitClick);
+
+            //Setting labels and text boxes size and location
             //Employee ID
             lblID.Text = "Employee ID";
-            lblID.Location = new Point(10,42);
+            lblID.Location = new Point(20,52);
             lblID.Size = new Size(100,20);
     
-            txbID.Location = new Point(120,40);
+            txbID.Location = new Point(120,50);
             txbID.Size = new Size(120,20);
             
             //First name 
             lblFname.Text = "First Name";
-            lblFname.Location = new Point(10,82);
+            lblFname.Location = new Point(20,92);
             lblFname.Size = new Size(100,20);
             
-            txbFname.Location = new Point(120,80);
+            txbFname.Location = new Point(120,90);
             txbFname.Size = new Size(120,20);
 
             //Last name
             lblLname.Text = "Last Name";
-            lblLname.Location = new Point(10,122);
+            lblLname.Location = new Point(20,132);
             lblLname.Size = new Size(100,20);
             
-            txbLname.Location = new Point(120,120);
+            txbLname.Location = new Point(120,130);
             txbLname.Size = new Size(120,20);
 
             //Address 
             lblAddress.Text = "Address";
-            lblAddress.Location = new Point(10,162);
+            lblAddress.Location = new Point(20,172);
             lblAddress.Size = new Size(100,20);
             
-            txbAddress.Location = new Point(120,160);
+            txbAddress.Location = new Point(120,170);
             txbAddress.Size = new Size(250,20);
 
             //Postcode
             lblPcode.Text = "Postcode";
-            lblPcode.Location = new Point(10,202);
+            lblPcode.Location = new Point(20,212);
             lblPcode.Size = new Size(100,20);
             
-            txbPcode.Location = new Point(120,200);
+            txbPcode.Location = new Point(120,210);
             txbPcode.Size = new Size(120,20);
             
             //DoB
             lblDOB.Text = "DOB";
-            lblDOB.Location = new Point(10,242);
+            lblDOB.Location = new Point(20,252);
             lblDOB.Size = new Size(100,20);
             
-            txbDOB.Location = new Point(120,240);
+            txbDOB.Location = new Point(120,250);
             txbDOB.Size = new Size(120,20);
 
             //Gender
             lblGender.Text = "Gender";
-            lblGender.Location = new Point(10,282);
+            lblGender.Location = new Point(20,292);
             lblGender.Size = new Size(100,20);
             
-            txbGender.Location = new Point(120,280);
+            txbGender.Location = new Point(120,290);
             txbGender.Size = new Size(120,20);
 
             //Photo
             lblPhoto.Text = "Photo";
-            lblPhoto.Location = new Point(10,322);
+            lblPhoto.Location = new Point(20,332);
             lblPhoto.Size = new Size(100,20);
             
-            txbPhoto.Location = new Point(120,320);
+            txbPhoto.Location = new Point(120,330);
             txbPhoto.Size = new Size(250,20);
 
             //Document
             lblDoc.Text = "Document";
-            lblDoc.Location = new Point(10,362);
+            lblDoc.Location = new Point(20,372);
             lblDoc.Size = new Size(100,20);
             
-            txbDoc.Location = new Point(120,360);
+            txbDoc.Location = new Point(120,370);
             txbDoc.Size = new Size(250,20);
+
             
             //Adding elements to win form
-
             this.Controls.Add(topPanel);
-
             this.Controls.Add(mainPanel);
 
+            // adding buttons in top panel
             topPanel.Controls.Add(btnEdit);
             topPanel.Controls.Add(btnView);
 
+            // adding labels in main panel
             mainPanel.Controls.Add(lblID);
             mainPanel.Controls.Add(lblFname);
             mainPanel.Controls.Add(lblLname);
@@ -161,6 +186,7 @@ namespace EDApp{
             mainPanel.Controls.Add(lblPhoto);
             mainPanel.Controls.Add(lblDoc);  
 
+            // adding text field in main panel 
             mainPanel.Controls.Add(txbID);
             mainPanel.Controls.Add(txbFname);
             mainPanel.Controls.Add(txbLname);
@@ -170,6 +196,12 @@ namespace EDApp{
             mainPanel.Controls.Add(txbGender);
             mainPanel.Controls.Add(txbPhoto);
             mainPanel.Controls.Add(txbDoc);
+
+            // adding function buttons in main panel
+            mainPanel.Controls.Add(btnAdd);
+            mainPanel.Controls.Add(btnDelete);
+            mainPanel.Controls.Add(btnExit);
+
         }
         
         //Edit button event handler   
@@ -178,13 +210,12 @@ namespace EDApp{
             menuFrame();
         } 
 
-
         //View button event handler
         public void btnViewClick(object sender, EventArgs e) 
         {
             mainPanel.Visible = false;
             topPanel.Visible = true;
         }  
-           
-    }
+
+        }
 }
